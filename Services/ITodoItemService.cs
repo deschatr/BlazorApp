fe48b5d2aa80ad35ac1b20616c452c9dfe6d5451
deschatr <nicolas@lappis.com>
2023-03-29 17:58:23 +0100

    del and put debugged

--- Services/ITodoItemService.cs
+++ Services/ITodoItemService.cs
@@ -2,4 +2,6 @@ public interface ITodoItemService
 {
     Task<IEnumerable<TodoItem>> GetTodoItems();
     Task<IEnumerable<TodoItem>> AddTodoItem(TodoItem todoItem);
+    Task<IEnumerable<TodoItem>> DeleteTodoItem(TodoItem todoItem);
+    Task<IEnumerable<TodoItem>> SetTodoItem( TodoItem todoItem );
 }
