638371d93489ee358fb998c2df3c3d01f170451a
deschatr <nicolas@lappis.com>
2023-04-01 02:14:53 +0100

    removed uncesseary Json attributes

--- Data/TodoItem.cs
+++ Data/TodoItem.cs
@@ -3,12 +3,9 @@ using System.Text.Json.Serialization;
 // defines the TodoItem class, and associated JSON attributes
 public class TodoItem
 {
-    [JsonPropertyName("id")]
     public long Id { get; set; }
 
-    [JsonPropertyName("name")]
     public string? Name { get; set; }
     
-    [JsonPropertyName("isComplete")]
     public bool IsComplete { get; set; } = false;
 }
